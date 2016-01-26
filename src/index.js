c1efe74f100820d9583ec7f4a5565042d55aaf37
Will Soto <will.soto9@gmail.com>
2016-01-25 19:29:32 -0500

    feat: add support for styles, names and change the data structure

--- src/index.js
+++ src/index.js
@@ -4,27 +4,29 @@ import _ from 'lodash';
 export default function(file) {
   var content = YAML.load(file);
 
-  let result = _.mapValues(content, (val) => {
-    val.scopes = _.map(val.scopes, (scope) => {
+  return _(content).map((val, hex) => {
+    return _.map(val.scopes, (scope) => {
       if (_.isString(scope)) {
-        let obj = {};
-
-        obj[scope] = {
-          styles: {
-            fontStyle: []
+        return {
+          name: null,
+          scope,
+          settings: {
+            foreground: hex
           }
         };
+      } else {
+        let index = _(scope).keys().first();
+        let {fontStyle, name} = scope[index];
 
-        return obj;
+        return {
+          name: name,
+          scope: index,
+          settings: {
+            fontStyle,
+            foreground: hex
+          }
+        };
       }
-
-      return scope;
     });
-
-    return val;
-  });
-
-  // console.log(JSON.stringify(result, null, 2));
-
-  return result;
+  }).flatten().value();
 }
