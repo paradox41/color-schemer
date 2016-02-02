035b07ec92cc0b21bbc3209497e49db7da25b75e
Will Soto <will.soto9@gmail.com>
2016-02-01 19:58:31 -0500

    feat(sublime): add rendering for sublime

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
 import YAML from 'yamljs';
 import _ from 'lodash';
 
-export default function(file) {
+import Mustache from 'mustache';
+import uuid from 'node-uuid';
+
+export function colorSchemeParser(file) {
   var content = YAML.load(file);
 
   var settings = _(content).map((val, hex) => {
     return _.map(val.scopes, (scope) => {
       if (_.isString(scope)) {
         return {
-          name: null,
+          name: makeName(scope),
           scope: scope,
           settings: {
             foreground: hex
@@ -19,7 +25,7 @@ export default function(file) {
         let {fontStyle, name} = scope[key];
 
         return {
-          name: name || null,
+          name: name || makeName(key),
           scope: key,
           settings: {
             fontStyle: fontStyle || null,
@@ -30,8 +36,37 @@ export default function(file) {
     });
   }).flatten().value();
 
+  var name = _.get(content, 'name', '');
+  var style = _.get(content, 'style', 'light').toLowerCase();
+  var semanticClass = name.split(' ').join('_').toLowerCase();
+
   return {
-    name: _.get(content, 'name', null),
-    settings: settings
+    name: name,
+    background: _.get(content, 'background', ''),
+    caret: _.get(content, 'caret', ''),
+    foreground: _.get(content, 'foreground', ''),
+    invisibles: _.get(content, 'invisibles', ''),
+    lineHighlight: _.get(content, 'lineHighlight', ''),
+    selection: _.get(content, 'selection', ''),
+    settings: settings,
+    uuid: uuid.v4(),
+    semanticClass: `theme.${style}.${semanticClass}`
   };
+}
+
+export function renderSublime(context) {
+  let template = loadTemplate(path.resolve(__dirname + '/templates/sublime.mustache'));
+
+  return Mustache.render(template, context);
+}
+
+/**
+ * Helpers
+ */
+function makeName(scope) {
+  return scope.split('.').map(_.upperFirst).join(' ');
+}
+
+function loadTemplate(template) {
+  return fs.readFileSync(template, 'utf-8');
 }
--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 import test from 'tape';
-import colorSchemeParser from '../src';
+import {colorSchemeParser, renderSublime} from '../src';
 
 test('colorSchemeParser', (t) => {
   t.plan(1);
+  let file = `${__dirname}/fixtures/scheme.yml`;
 
-  colorSchemeParser(`${__dirname}/fixtures/scheme.yml`);
+  let context = colorSchemeParser(file);
+
+  console.log(renderSublime(context));
 
   t.equal(true, true);
 });
